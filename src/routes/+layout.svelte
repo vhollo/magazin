a3225e1c5d673f9b6c1d7dfb8ccdd0c6d4a38bab
vhollo <vhollo@protonmail.com>
2025-01-17 18:47:53 +0100

    svelte5 & MiniSearch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '../app.postcss'
+	import './app.postcss'
   import Nav from '$lib/components/Nav.svelte'
   import Footer from '$lib/components/Footer.svelte'
   //import { page } from '$app/stores'
