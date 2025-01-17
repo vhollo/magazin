12b9b8ae5c4c43f04142d59150998e8213146191
vhollo <vhollo@protonmail.com>
2025-01-17 19:43:13 +0100

    svelte5 & MiniSearch

--- src/lib/vercel/pg.server.ts
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-import { neon } from '@neondatabase/serverless';
-
-const connectionString: string = process.env.PG_URL as string;
-
-const sql = neon(connectionString);
-export { sql };
