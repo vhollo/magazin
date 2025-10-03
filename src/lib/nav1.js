abd4a99ccb7cd6de1ba3c51b66210d1ed7bd3383
vhollo <vhollo@protonmail.com>
2025-10-03 23:51:52 +0200

    Gyógyszertárkereső

--- src/lib/nav1.js
+++ src/lib/nav1.js
@@ -2,10 +2,11 @@ export const nav1 = {
   'Hírek': '/hirek',
   'DiabKVÍZ': '/kviz',
   'Előfizetés': '/elofizetes',
-  'Kapcsolat': {
+  'Partnereink': {
     'Alapítvány a Cukorbetegekért': '/alapitvany',
     'Tudomány Kiadó': 'https://www.tudomany-kiado.hu',
     'CEOSZ': 'https://ceosz.hu',
+    'Gyógyszertárkereső': '/patika'
   },
   // 'Admin': {
   //   'Hird': '/admin',
